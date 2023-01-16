6f7cdbbf8d67739e3f6c3111d49bd5501ac601f7
Denis Kurilyov <otrawaotrawa@gmail.com>
2023-01-17 01:31:24 +0200

    exercise 3.3 done

--- index.js
+++ index.js
@@ -30,6 +30,15 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    person
+        ? res.json(person)
+        : res.status(404).end()
+})
+
 app.get('/info', (req, res) => {
     const personsCount = persons.length
     const date = new Date
