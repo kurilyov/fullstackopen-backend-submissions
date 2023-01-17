2edb5830c3be69fdbf39536c42854935d32d99af
Denis Kurilyov <otrawaotrawa@gmail.com>
2023-01-17 02:07:08 +0200

    exercise 3.7 done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 const PORT = 3001
 
